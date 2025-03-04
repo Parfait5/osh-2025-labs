cb718568b0753d02c49230cb887681f8135b496c
Parfait5 <yuhaoxiang@mail.ustc.edu.cn>
2025-03-04 19:45:08 +0800

    add : ignore or not

---
+++ lab0/build/CMakeFiles/bubble_sort.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bubble_sort.dir/bubblesort.cpp.o"
+  "CMakeFiles/bubble_sort.dir/bubblesort.cpp.o.d"
+  "CMakeFiles/bubble_sort.dir/main.cpp.o"
+  "CMakeFiles/bubble_sort.dir/main.cpp.o.d"
+  "bubble_sort"
+  "bubble_sort.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bubble_sort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
