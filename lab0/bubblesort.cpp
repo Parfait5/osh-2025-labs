f1f07bfd83f1db52d0979e0e7922001e1bc300bb
Parfait5 <yuhaoxiang@mail.ustc.edu.cn>
2025-03-04 11:56:34 +0800

    finish : makefile

--- lab0/bubblesort.cpp
+++ lab0/bubblesort.cpp
@@ -7,6 +7,14 @@ void bubbleSort(std::vector<int>& arr) {
         for (int j = 0; j < n - i - 1; ++j) {
             if (arr[j] > arr[j + 1]) {
                 std::swap(arr[j], arr[j + 1]);
+                
+                #ifdef DEBUG
+                std::cout << "Swap detected: ";
+                for (const auto& num : arr) {
+                    std::cout << num << " ";
+                }
+                std::cout << std::endl;
+                #endif
             }
         }
     }
