6537e2747c0fad94d05d5ffa41a77e1ba8f64fe6
Parfait5 <yuhaoxiang@mail.ustc.edu.cn>
2025-04-15 15:00:42 +0800

    lab2: add tokenize

--- lab2/shell/src/main.rs
+++ lab2/shell/src/main.rs
@@ -14,18 +14,26 @@ fn prompt() -> Option<()> {
     let cwd = env::current_dir().ok()?;
     let home = env::var("HOME").unwrap_or_default();
     let home_path = Path::new(&home); // 将字符串转为 Path
-    let path = if cwd == home_path {
-        '~'.to_string
-    }else if cwd.starts_with(&home_path){
-        "~/".to_string() + cwd.strip_prefix(&home).ok()?.to_str()?
-    }else{
-        cwd.to_str?.to_string
-    };
+    let path = 
+        if cwd == home_path {
+            '~'.to_string
+        }else if cwd.starts_with(&home_path){
+            "~/".to_string() + cwd.strip_prefix(&home).ok()?.to_str()?
+        }else{
+            cwd.to_str?.to_string
+        };
     print!("{}>",&path);
     io::stdout().flush().ok()?;
     Some(());
 }
 
-fn tokenize() ->Option<Vec(String)>{
-    // to do
+fn tokenize(command:String) ->Option<Vec(String)>{
+    command.split_whiteapce()
+    .map(|token|{
+        if token.starts_with('~'){
+            env::var("HOME").unwrap_or_default()+token.strip_prefix('~').unwrap()
+        }else if token.starts_with('$'){
+            env::var(token.strip_prefix('$').unwrap()).unwrap_or_default()
+        }else{token.to_string()}
+    }).collect()
 }
