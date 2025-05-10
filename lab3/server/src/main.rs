8173be989b07ebc73f70431e8d762907fd822b4c
Parfait5 <yuhaoxiang@mail.ustc.edu.cn>
2025-05-10 21:36:21 +0800

    lab: main

--- lab3/server/src/main.rs
+++ lab3/server/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::path::Path;
+
+fn main(){
+    // 绑定地址并监听
+    let listener = TcpListener::bind("0.0.0.0:8000").expect("Failed to bind address");
+    println!("Listening on http://0.0.0.0:8000");
 }
